cdc54d3f8ec1a283864fc0cab457545347d8f45d
adeshnimase15 <140651643+adeshnimase15@users.noreply.github.com>
2025-04-24 14:55:33 +0530

    yeda

--- simple-java-app/src/Main.java
+++ simple-java-app/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Adesh naveen and abhi!");
+        System.out.println("Hello Adesh naveen and abhi 123456+7845645!");
     }
 }
 
