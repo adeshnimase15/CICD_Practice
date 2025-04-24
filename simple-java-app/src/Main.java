092dad296f87b89b2ea8fb71a0bcd82e00ddce0e
adeshnimase15 <140651643+adeshnimase15@users.noreply.github.com>
2025-04-24 14:51:15 +0530

    navin

--- simple-java-app/src/Main.java
+++ simple-java-app/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from a GitHub-hosted Java project built by Jenkins!");
+        System.out.println("Hello Adesh naveen and abhi!");
     }
 }
 
