6d381cf19ce56910d33a298588d2e48a3e03212b
adeshnimase15 <140651643+adeshnimase15@users.noreply.github.com>
2025-04-24 14:44:17 +0530

    Update Main.java

--- simple-java-app/src/Main.java
+++ simple-java-app/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from a GitHub-hosted Java project built by Jenkins!");
         System.out.println("Gpp re zatuuuu");
+        System.out.println("Yedzavya re zatuuuu");
     }
 }
 
